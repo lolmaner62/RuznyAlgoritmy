4ebc406bdcb34c44ba27857a61f475d9411e09d8
Stepan_Skolni <stepa.sula1@gmail.com>
2025-01-24 12:13:13 +0100

    Skibidi1

---
+++ RuznyAlgoritmy/Algoritmy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace RuznyAlgoritmy
+{
+    static public class Algoritmy
+    {
+        static List<int> Vygeneruj()
+        {
+            Console.WriteLine("Kolik chcete cisel?");
+            int pocet = int.Parse(Console.ReadLine());
+            Random rng = new Random();
+            List<int> output = new List<int>();
+            for (int i = 0; i < pocet; i++)
+            {
+                output[i] = rng.Next(0, 1001);
+                Console.WriteLine(output[i]);
+            }
+            
+            return output;
+        }
+        static bool ChcetePokracovat()
+        {
+            Console.WriteLine("Chcete pokračovat? \n1: Ano \n2: Ne (Nebo cokoliv kromě jedničky :D");
+            if (Console.ReadLine() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static public bool Bez_Zarazky()
+        {
+            List<int> pole = Vygeneruj();
+            Console.WriteLine("Ktere cislo hledame:");
+            int hledane = int.Parse(Console.ReadLine());
+            int nalezene = 0;
+            foreach (var item in pole)
+            {
+
+                if (item == hledane)
+                {
+                    nalezene = 1;
+
+                }
+            }
+            if (nalezene == 1)
+            {
+                Console.WriteLine("Nalezeno");
+            }
+            else
+            {
+                Console.WriteLine("Nenalezeno");
+            }
+            return ChcetePokracovat();
+        }
+        static public bool Se_Zarazkou()
+        {
+            List<int> pole = Vygeneruj();
+            Console.WriteLine("Ktere cislo hledame:");
+            int hledane = int.Parse(Console.ReadLine());
+            int nalezene = 0;
+            foreach (var item in pole)
+            {
+
+                if (item == hledane)
+                {
+                    nalezene = 1;
+                    break;
+                }
+            }
+            if (nalezene == 1)
+            {
+                Console.WriteLine("Nalezeno");
+            }
+            else
+            {
+                Console.WriteLine("Nenalezeno");
+            }
+            return ChcetePokracovat();
+        }
+        static public bool Binarni()
+        {
+            List<int> pole = Vygeneruj();
+            Console.WriteLine("Ktere cislo hledame:");
+            int hledane = int.Parse(Console.ReadLine());
+            int nalezene = 0;
+            pole.Sort();
+            int Left = 0;
+            int right = pole.Count;
+
+            return ChcetePokracovat();
+        }
+
+    }
+}
--- RuznyAlgoritmy/Program.cs
+++ RuznyAlgoritmy/Program.cs
@@ -6,6 +6,7 @@ Při spuštění se zobrazí nabídka kde bude 9 možností, 8 algoritmů , 9 ex
 Vybrána nabídka, zeptá se na počet prvků v poli, poté zobrazí pole, pokud zvolí řadící algoritmus, zobrazí 
 seřazené pole, pokud hledací algoritmus, zeptá se na číslo které hledá, poté napíše jestli číslo je nebo ne
 zeptá jestli chce pokračovat nebo ne, poté nabídka 卐卐卐卐卐卐卐卐卐卐卐卐卐
+Od 0 do 1000
 */
 
 
@@ -17,15 +18,63 @@ namespace RuznyAlgoritmy
 {
     class Program
     {
+        
+        static bool tocka = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Algoritmy 3000");
-        }
+            while(tocka)
+            {
+                switch (ZobrazNabidku())
+                {
+                    case 1:
+                        tocka = Algoritmy.Bez_Zarazky();
+                        break;
+                    case 2:
+                        tocka = Algoritmy.Binarni();
+                        break;
 
+
+
+
+
+                    case 4:
+                        Algoritmy.Se_Zarazkou();
+                        break;
+                    case 9:
+                        Environment.Exit(0);
+                        break;
+                    case 69:
+                        continue;
+                    default:
+                        Console.WriteLine("Jak????");
+                        break;
+                }
+            }
+            Environment.Exit(0);
+            
+        }
+        
         static int ZobrazNabidku()
         {
-            
-
+            Console.WriteLine("Hledaci algoritmy: \n1: Bez zarazky \n2: Binarni \n3: Nainvni\n4: Se zarazkou");
+            Console.WriteLine("Radici algoritmy:\n5: Bubble sort\n6: Insert\n7:Quick Sort\n8:Select sort \n9: Exit");
+            if (int.TryParse(Console.ReadLine(), out int Odpoved))
+            {
+                if (Odpoved <= 9 && Odpoved >= 1)
+                {
+                    return Odpoved;
+                }
+                else
+                {
+                    Console.WriteLine("Musi byt ve spravnem rozmezi opicaku");
+                    return 69;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Musis napsat cislo opicaku");
+                return 69;
+            }
         }
 
     }
