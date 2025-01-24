1f2e312719cf3e3a19e4ee4f6925eff1edad6dae
Stepan_Skolni <stepa.sula1@gmail.com>
2025-01-24 11:19:15 +0100

    Přidejte soubory projektu.

---
+++ RuznyAlgoritmy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+
+Při spuštění se zobrazí nabídka kde bude 9 možností, 8 algoritmů , 9 exit
+Vybrána nabídka, zeptá se na počet prvků v poli, poté zobrazí pole, pokud zvolí řadící algoritmus, zobrazí 
+seřazené pole, pokud hledací algoritmus, zeptá se na číslo které hledá, poté napíše jestli číslo je nebo ne
+zeptá jestli chce pokračovat nebo ne, poté nabídka 卐卐卐卐卐卐卐卐卐卐卐卐卐
+*/
+
+
+
+
+
+
+namespace RuznyAlgoritmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmy 3000");
+        }
+
+        static int ZobrazNabidku()
+        {
+            
+
+        }
+
+    }
+}
